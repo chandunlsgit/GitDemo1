ebb76db9084fc6a05df161d23785e3c3a2c08e8d
CHANDU-NLS <CHANDU-NLS@LAPTOP-FMDVMUCQ>
2022-03-09 19:00:53 +0530

    first local repo commit

---
+++ src/com/nls/git/GitDemoClass.java
@@ -0,0 +1,10 @@
+package com.nls.git;
+
+public class GitDemoClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   System.out.println("First Demo statements");
+	}
+
+}
