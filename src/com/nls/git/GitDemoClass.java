4da86178194a369971e11ddce1e0e2e5b11ed610
CHANDU-NLS <CHANDU-NLS@LAPTOP-FMDVMUCQ>
2022-03-29 16:56:33 +0530

    added second print statmentes

--- src/com/nls/git/GitDemoClass.java
+++ src/com/nls/git/GitDemoClass.java
@@ -5,6 +5,7 @@ public class GitDemoClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
    System.out.println("First Demo statementss");
+   System.out.println("second Demo statementss");
 	}
 
 }
