294ffbadb065e5677155d9b89ec45af5c96bf23f
CHANDU-NLS <CHANDU-NLS@LAPTOP-FMDVMUCQ>
2022-03-29 17:02:14 +0530

    FOURHT COMMIT

--- src/com/nls/git/GitDemoClass.java
+++ src/com/nls/git/GitDemoClass.java
@@ -4,8 +4,10 @@ public class GitDemoClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-   System.out.println("First Demo statementss");
+   System.out.println("First Demo ");
    System.out.println("second Demo statementss");
+   System.out.println("THIRD Demo statementss");
+   System.out.println("FOURHT Demo statementss");
 	}
 
 }
